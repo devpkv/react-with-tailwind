75127255b6128c9513b497ff43f1d95b2543f00f
Pankaj Kumar <mailtokpankaj@gmail.com>
2025-06-26 10:18:05 +0530

    updated the anchor tag to react tag

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+
 function Nav() {
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <a href="/" className="text-white text-lg font-bold">MyApp</a>
+        <Link to="/" className="text-white text-lg font-bold">MyApp</Link>
         <ul className="flex space-x-4">
-          <li><a href="/" className="text-gray-300 hover:text-white">Home</a></li>
-          <li><a href="/about" className="text-gray-300 hover:text-white">About</a></li>
-          <li><a href="/contact" className="text-gray-300 hover:text-white">Contact</a></li>
+          <li><Link to="/" className="text-gray-300 hover:text-white">Home</Link></li>
+          <li><Link to="/about" className="text-gray-300 hover:text-white">About</Link></li>
+          <li><Link to="/contact" className="text-gray-300 hover:text-white">Contact</Link></li>
         </ul>
       </div>
     </nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ function Home()
         <div>
             <h1>Welcome to the Home Page</h1>
             <p>This is the main page of our application.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, praesentium?</p>
         </div>
     );
 }
